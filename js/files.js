e047c2d26731f143d951a2ee1d2cee9cf57ee3d3
AminaNasra <nasraaar2020@gmail.com>
2025-05-26 16:32:13 +1000

    Algorithm for writing and reading from a file
    Added in file comments.
    Added in Algorithm for writing to a file.
    Added in Algorithm for reading to a file.

---
+++ js/files.js
@@ -0,0 +1,35 @@
+// file: files.js
+// desc: This file shows saving a text file algorith, reading a text file algorithm. writing to a text file and reading from a text file
+// Author: Amina Aar
+// Last modified: 20/05/2025
+// version 1.0.0
+
+// An algorithm for saving a string to a text file
+// 1- BEGIN
+// 2-    IMPORT 'fs' module
+// 2-    DEFINE 'filePath' as the path of the file to write
+// 3-    DEFINE 'data' – The data you want to save to the file.
+// 4-    USE 'fs.writeFile method
+// 5-       PASS 'filePath' and 'data' as arguments
+// 6-       PROVIDE a callback function to handle errors
+// 7-          IF error occurs
+// 8-             PRINT error message
+// 9-          ELSE
+// 10-             PRINT success message
+// 11- END
+
+// An algorithm for reading in a text file
+/*
+  1- BEGIN
+  2-   IMPORT 'fs' module
+  3-   DEFINE 'filePath' as the path of the file to read
+  4-   USE 'fs.readFile' method
+  5-      PASS 'filePath' as an argument
+  6-      PROVIDE a callback function to handle errors and fileText
+  7-         IF error occurs
+  8-            PRINT error message
+  9-         ELSE
+  10-           PRINT file content in string format
+  11- END
+
+*/
